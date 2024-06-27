242caa2584429a760a0e11ee94139420f4cbe575
Maxitoto <39698863+maxitoto@users.noreply.github.com>
2024-06-27 05:20:06 -0300

    Delete djangomodprof/Repelis directory

--- djangomodprof/Repelis/fuente/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Critica
-
-class Criticar(forms.ModelForm):
-    class Meta:
-        model = Critica
-        fields = ['nombre', 'correo', 'comentario','puntaje']
-
-   
